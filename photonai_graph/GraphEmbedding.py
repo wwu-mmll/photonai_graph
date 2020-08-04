ccda1041ff9af72668ee06746d4b32d57ae97d5e
BenisFarmen <45595667+BenisFarmen@users.noreply.github.com>
2020-08-04 13:49:26 +0200

    Update GraphEmbedding.py
    Documented Graph Embeddings methods

--- photonai_graph/GraphEmbedding.py
+++ photonai_graph/GraphEmbedding.py
@@ -35,8 +35,39 @@ from gem.embedding.sdne import SDNE
 class GraphEmbeddingGraphFactorization(BaseEstimator, TransformerMixin):
     _estimator_type = "transformer"
 
-    # threshold a matrix to generate the adjacency matrix
-    # you can use both a different and the own matrix
+    """
+    Transformer class for calculating a Graph Embedding
+    based on Graph Factorization (Ahmed et al., 2013).
+    Graph Factorization factorizes the adjacency matrix
+    with regularization. Implementation based on gem
+    python package.
+    
+
+    Parameters
+    ----------
+    * `embedding_dimension` [int, default=1]:
+        the number of dimensions that the final embedding will have
+    * `maximum_iterations` [int, default=10000]
+	the number of iterations used in sgd, when learning the embedding
+    * `learning_rate` [float, default=1 * 10 ** -4]
+	the learning rate of sgd
+    * `regularization_coefficient` [float, default=1.0]
+	the regularization coefficient for regulating the magnitude of the weights
+    * `adjacency_axis` [int, default=0]:
+        position of the adjacency matrix, default being zero
+    * `verbosity` [int, default=0]:
+        The level of verbosity, 0 is least talkative and gives only warn and error, 1 gives adds info and 2 adds debug
+    * `logs` [str, default=None]:
+        Path to the log data
+    
+    
+
+    Example
+    -------
+        constructor = GraphEmbeddingGraphFactorization(maximum_iterations=20000,
+                            			       regularization_coefficient=0.5)
+    """
+
 
     def __init__(self, embedding_dimension = 1,
                  maximum_iterations = 10000, learning_rate = 1 * 10 ** -4,
@@ -80,7 +111,34 @@ class GraphEmbeddingGraphFactorization(BaseEstimator, TransformerMixin):
 class GraphEmbeddingHOPE(BaseEstimator, TransformerMixin):
     _estimator_type = "transformer"
 
-    # HOPE always generates a 2-dimensional output which then has to be flattened
+    """
+    Transformer class for calculating a Graph Embedding
+    based on Higher-order proximity preserved embedding
+    (Mingdong et al., 2016).
+    Implementation based on gem python package.
+    
+
+    Parameters
+    ----------
+    * `embedding_dimension` [int, default=1]:
+        the number of dimensions that the final embedding will have
+    * `decay_factor` [ifloat, default=0.01]
+	the higher order coefficient beta
+    * `adjacency_axis` [int, default=0]:
+        position of the adjacency matrix, default being zero
+    * `verbosity` [int, default=0]:
+        The level of verbosity, 0 is least talkative and gives only warn and error, 1 gives adds info and 2 adds debug
+    * `logs` [str, default=None]:
+        Path to the log data
+    
+    
+
+    Example
+    -------
+        constructor = GraphEmbeddingHOPE(embedding_dimension=1,
+                            		 decay_factor=0.1)
+    """
+
 
     def __init__(self, embedding_dimension = 1,
                  decay_factor = 0.01,
@@ -129,7 +187,31 @@ class GraphEmbeddingHOPE(BaseEstimator, TransformerMixin):
 class GraphEmbeddingLaplacianEigenmaps(BaseEstimator, TransformerMixin):
     _estimator_type = "transformer"
 
-    # HOPE always generates a 2-dimensional output which then has to flattened
+    """
+    Transformer class for calculating a Graph Embedding
+    based on Laplacian Eigenmaps (Belkin & Niyogi, 2013).
+    Implementation based on gem python package.
+    
+
+    Parameters
+    ----------
+    * `embedding_dimension` [int, default=1]:
+        the number of dimensions that the final embedding will have
+    * `adjacency_axis` [int, default=0]:
+        position of the adjacency matrix, default being zero
+    * `verbosity` [int, default=0]:
+        The level of verbosity, 0 is least talkative and gives only warn and error, 1 gives adds info and 2 adds debug
+    * `logs` [str, default=None]:
+        Path to the log data
+    
+    
+
+    Example
+    -------
+        constructor = GraphEmbeddingLaplacianEigenmaps(embedding_dimension=1,
+                            			       decay_factor=0.1)
+    """
+
 
     def __init__(self, embedding_dimension = 1,
                  decay_factor = 0.01,
@@ -171,7 +253,31 @@ class GraphEmbeddingLaplacianEigenmaps(BaseEstimator, TransformerMixin):
 class GraphEmbeddingLocallyLinearEmbedding(BaseEstimator, TransformerMixin):
     _estimator_type = "transformer"
 
-    # A Locally Linear Embedding returns a photonai_graph embedding that is equal to the number of nodes
+    """
+    Transformer class for calculating a Graph Embedding
+    based on Locally Linear Embedding (Roweis & Saul, 
+    2000).
+    Implementation based on gem python package.
+    
+
+    Parameters
+    ----------
+    * `embedding_dimension` [int, default=1]:
+        the number of dimensions that the final embedding will have
+    * `adjacency_axis` [int, default=0]:
+        position of the adjacency matrix, default being zero
+    * `verbosity` [int, default=0]:
+        The level of verbosity, 0 is least talkative and gives only warn and error, 1 gives adds info and 2 adds debug
+    * `logs` [str, default=None]:
+        Path to the log data
+    
+    
+
+    Example
+    -------
+        constructor = GraphEmbeddingLaplacianEigenmaps(embedding_dimension=1,
+                            			       decay_factor=0.1)
+    """
 
     def __init__(self, embedding_dimension = 1,
                  decay_factor = 0.01,
@@ -210,8 +316,53 @@ class GraphEmbeddingLocallyLinearEmbedding(BaseEstimator, TransformerMixin):
 class GraphEmbeddingSDNE(BaseEstimator, TransformerMixin):
     _estimator_type = "transformer"
 
-    # HOPE always generates a 2-dimensional output which then has to flattened
+    """
+    Transformer class for calculating a Graph Embedding
+    based on Structural Deep Network Embedding (Wang, Cui
+    & Zhu, 2016).
+    Implementation based on gem python package.
+    
 
+    Parameters
+    ----------
+    * `embedding_dimension` [int, default=1]:
+        the number of dimensions that the final embedding will have
+    * `seen_edge_reconstruction_weight` [int, default=5]
+	the penalty parameter beta in matrix B of the 2nd order objective
+    * `first_order_proximity_weight` [float, default=1e-5]
+	the weighing hyperparameter alpha for the 1st order objective
+    * `lasso_regularization_coefficient` [float, default=1e-6]
+	the L1 regularization coefficient
+    * `ridge_regression_coefficient` [float, default=1e-6]
+	the L2 regularization coefficient
+    * `number_of_hidden_layers` [int, default=3]
+	the number of hidden layers in the encoder/decoder
+    * `layer_sizes` [int, default=[50, 15,]]
+	the number of units per layer in the hidden layers of the encoder/decoder. Vector of length number_of_hidden_layers
+	-1
+    * `num_iterations` [int, default=50]
+	the number of iterations with which to train the network
+    * `learning_rate` [float, default=0.01]
+	the learning rate with which the network is trained
+    * `batch_size` [int, default=500]
+	the batch size when training the algorithm
+    * `adjacency_axis` [int, default=0]:
+        position of the adjacency matrix, default being zero
+    * `verbosity` [int, default=0]:
+        The level of verbosity, 0 is least talkative and gives only warn and error, 1 gives adds info and 2 adds debug
+    * `logs` [str, default=None]:
+        Path to the log data
+    
+    
+
+    Example
+    -------
+        constructor = GraphEmbeddingSDNE(embedding_dimension=1,
+                            		 seen_edge_reconstruction_weight=10,
+					 first_order_proximity_weight=1e-4
+					 num_hidden_layers=5,
+					 layer_sizes=[50, 25, 20, 15,])
+    """
     def __init__(self, embedding_dimension = 1,
                  seen_edge_reconstruction_weight = 5,
                  first_order_proximity_weight = 1e-5,
