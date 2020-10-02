8905eeadf83d0322420384a74a622b515863e8c6
BenisFarmen <45595667+BenisFarmen@users.noreply.github.com>
2020-10-02 11:45:24 +0200

    Fix setup requirements
    Fixed requirements in the setup.py

--- setup.py
+++ setup.py
@@ -23,14 +23,14 @@ within the PHOTON framework. The module provides a range of graph machine learni
     download_url='https://github.com/wwu-mmll/photonai_graph/archive/' + __version__ + '.tar.gz',
     keywords=['machine learning', 'deep learning', 'graph convolutional neural networks', 'graphs'],
     classifiers=[],
-    install_requires=['photonai',
-                      'networkx',
-                      'pydot',
-                      'gem @ git+https://github.com/palash1992/GEM.git#egg=gem',
-                      'grakel @ git+https://github.com/ysig/GraKeL.git#egg=grakel'
-                      'torch',
-                      'dgl',
-                      'pandas',
-                      'numpy',
-                      'scipy']
+    install_requires=['photonai>=2.0.0',
+                      'networkx>=2.4',
+                      'pydot>=1.4.1',
+                      'gem @ git+https://github.com/palash1992/GEM.git@5da663255da28c433c42b296c8ceed7163f2d509',
+                      'grakel @ git+https://github.com/ysig/GraKeL.git@cfd14e0543075308d201327ac778a48643f81095'
+                      'torch>=1.6.0',
+                      'dgl==0.5.2',
+                      'pandas>=1.1.2',
+                      'numpy>=1.12.2',
+                      'scipy>=0.19.1']
 )
