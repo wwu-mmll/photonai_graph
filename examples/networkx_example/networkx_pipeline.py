279760d1c43ecaf4929ccdc6871c90cc5fcfcd18
Jan Ernsting <j.ernsting@uni-muenster.de>
2022-03-16 23:36:58 +0100

    Updated networkx example

--- examples/networkx_example/networkx_pipeline.py
+++ examples/networkx_example/networkx_pipeline.py
@@ -6,25 +6,22 @@ import numpy as np
 from photonai.base import Hyperpipe, PipelineElement
 from photonai_graph.GraphConversions import convert_graphs
 
-# create dataset of 20 graphs
-al_graphs = [barabasi_albert_graph(20, 3) for _ in range(500)]
+# create dataset of 1000 graphs
+rr_graphs = [erdos_renyi_graph(20, .25) for _ in range(500)]
 er_graphs = [erdos_renyi_graph(20, .2) for _ in range(500)]
-graphs = al_graphs + er_graphs
+graphs = rr_graphs + er_graphs
 
 # we have to transform the networkx graphs into numpy graphs before passing them to photon
 graphs = np.array(convert_graphs(graphs, output_format="dense"))
 graphs = np.expand_dims(graphs, axis=-1)
 
 # create labels
-ba_labels = [0] * 500
+rr_labels = [0] * 500
 er_labels = [1] * 500
-labels = ba_labels + er_labels
-
-# visualize the edge count for the graphs
-# plot_nx_edge_count(sparse_graphs, dense_graphs)
+labels = rr_labels + er_labels
 
 # Design your Pipeline
-my_pipe = Hyperpipe('basic_gcn_pipe',
+my_pipe = Hyperpipe('networkx_example_pipe',
                     inner_cv=KFold(n_splits=2),
                     optimizer='sk_opt',
                     optimizer_params={'n_configurations': 25},
