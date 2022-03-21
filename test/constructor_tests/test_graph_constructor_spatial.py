aded71e75aea119bd52433dc23d72b5c0fc52ba5
LeonardGustavo <v_hols01@uni-muenster.de>
2022-03-21 18:22:12 +0100

    Added the ho_atlas coords from Ktena et al and the added minor documentation

--- test/constructor_tests/test_graph_constructor_spatial.py
+++ test/constructor_tests/test_graph_constructor_spatial.py
@@ -11,6 +11,7 @@ class SpatialTests(unittest.TestCase):
         # get random matrix as feature matrix
         self.X4d = np.ones((20, 12, 12, 2))
         self.Xrandom4d = np.random.rand(20, 12, 12, 2)
+        self.XrandomHO4d = np.random.rand(20, 110, 110, 2)
         self.y = np.ones((20))
 
         # get test atlas
@@ -22,6 +23,18 @@ class SpatialTests(unittest.TestCase):
         # get check array
         arr = np.array([[3]])
         self.check_array = np.tile(arr, (1, 12))
+
+        # get check array for ho_atlas
+        self.ho_check = [4, 3, 4, 5, 5, 4, 3, 4, 3, 4, 5,
+                         4, 4, 5, 3, 3, 5, 5, 3, 4, 3, 3,
+                         4, 4, 4, 4, 3, 3, 3, 3, 3, 4, 4,
+                         3, 4, 4, 4, 3, 4, 3, 3, 3, 3, 3,
+                         3, 3, 3, 3, 3, 3, 4, 4, 3, 3, 4,
+                         4, 4, 4, 3, 3, 5, 5, 3, 4, 4, 3,
+                         6, 6, 4, 4, 4, 4, 3, 3, 5, 6, 6,
+                         7, 3, 4, 3, 3, 3, 3, 3, 4, 3, 3,
+                         4, 3, 4, 3, 4, 3, 4, 4, 4, 5, 4,
+                         5, 5, 4, 4, 4, 4, 4, 6, 4, 3, 3]
 
     def test_spatial(self):
         path = "/tmp/test_coords.csv"
@@ -41,3 +54,11 @@ class SpatialTests(unittest.TestCase):
         bool_array = np.count_nonzero(trans[0, :, :, 0], axis=1, keepdims=True) == self.check_array
         self.assertFalse(False in bool_array)
         os.remove(path)
+
+    def test_ho_atlas(self):
+        path = os.path.dirname(os.path.abspath(__file__))
+        g_constr = GraphConstructorSpatial(k_distance=3, atlas_name='ho', atlas_folder=path)
+        g_constr.fit(self.XrandomHO4d, self.y)
+        trans = g_constr.transform(self.XrandomHO4d)
+        ho_adj = trans[0, :, :, 0]
+        self.assertTrue(np.array_equal(np.count_nonzero(ho_adj, axis=0), self.ho_check))
