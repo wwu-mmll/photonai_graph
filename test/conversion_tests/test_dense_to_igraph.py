ad0c72d390eae54cda97f5909ce33fe013d2ad97
LeonardGustavo <v_hols01@uni-muenster.de>
2022-09-04 00:30:48 +0200

    Fixed igraph conversion tests

--- test/conversion_tests/test_dense_to_igraph.py
+++ test/conversion_tests/test_dense_to_igraph.py
@@ -1,7 +1,7 @@
 import unittest
 import numpy as np
 import igraph
-from photonai_graph.GraphConversions import dense_to_dgl
+from photonai_graph.GraphConversions import dense_to_igraph
 
 
 class DenseToIgraphTest(unittest.TestCase):
@@ -11,13 +11,13 @@ class DenseToIgraphTest(unittest.TestCase):
 
     def test_axis_argument(self):
         with self.assertRaises(NotImplementedError):
-            dense_to_dgl(self.random4d, feature_axis=1)
+            dense_to_igraph(self.random4d, feature_axis=1)
 
     def test_matrix(self):
-        g = dense_to_dgl(self.random4d, adjacency_axis=0)
+        g = dense_to_igraph(self.random4d, adjacency_axis=0)
         self.assertEqual(type(g[0]), igraph.Graph)
         self.assertEqual(type(g), list)
 
     def test_node_count(self):
-        g = dense_to_dgl(self.random4d, adjacency_axis=0)
+        g = dense_to_igraph(self.random4d, adjacency_axis=0)
         self.assertEqual(g[0].vcount(), 20)
