c224d40482bb0e7c6879891f314d8573d20d1877
LeonardGustavo <v_hols01@uni-muenster.de>
2022-09-22 20:50:07 +0200

    Added tests for networkx

--- test/measure_tests/test_measure_adapter.py
+++ test/measure_tests/test_measure_adapter.py
@@ -4,6 +4,7 @@ import numpy as np
 import networkx as nx
 import igraph
 from photonai_graph.Measures.MeasureAdapter import GraphMeasureAdapter
+from photonai_graph.util import NetworkxGraphWrapper
 
 
 class MeasureAdapterTests(unittest.TestCase):
@@ -12,6 +13,7 @@ class MeasureAdapterTests(unittest.TestCase):
         # generate random graphs
         self.random_mtrx = np.random.rand(10, 20, 20, 2)
         self.random_igraph = [igraph.Graph.Erdos_Renyi(n=10, p=0.3) for i in range(10)]
+        self.random_networkx = [nx.erdos_renyi_graph(n=10, p=0.3) for i in range(10)]
         # generate nonsense input
         self.edge_dict = {(1, 0): 1, (2, 0): 1, (3, 0): 1}
         self.y = np.random.rand(10)
@@ -32,6 +34,18 @@ class MeasureAdapterTests(unittest.TestCase):
         adapter = GraphMeasureAdapter(output='igraph')
         with self.assertRaises(TypeError):
             adapter.transform(self.edge_dict)
+
+    def test_np_to_networkx(self):
+        adapter = GraphMeasureAdapter(output='networkx')
+        adapter.fit(self.random_mtrx, self.y)
+        g = adapter.transform(self.random_mtrx)
+        self.assertEqual(type(g[0]), NetworkxGraphWrapper)
+        self.assertEqual(type(g), np.ndarray)
+
+    def test_networkx_to_networkx(self):
+        adapter = GraphMeasureAdapter(output='networkx')
+        g = adapter.transform(self.random_networkx)
+        self.assertEqual(type(g[0]), NetworkxGraphWrapper)
 
     def test_output_type_error(self):
         adapter = GraphMeasureAdapter(output='test')
