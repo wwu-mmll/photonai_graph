0e469182425e7f35c9eaf581c1ab11ea3402edfd
Jan Ernsting <j.ernsting@uni-muenster.de>
2023-02-15 14:47:49 +0100

    Fixed registry

--- photonai_graph/__init__.py
+++ photonai_graph/__init__.py
@@ -1,15 +1,31 @@
 import os
-from datetime import datetime
+from photonai.base import PhotonRegistry
 from photonai.photonlogger import logger
-from photonai_graph.version import __version__
 
-current_path = os.path.dirname(os.path.abspath(__file__))
-registered_file = os.path.join(current_path, "registered")
-logger.info("Checking Graph Module Registration")
-if not os.path.isfile(registered_file):  # pragma: no cover
-    logger.info("Registering Graph Module")
-    from photonai.base import PhotonRegistry
+from .version import __version__
+
+
+def do_register(current_path, registered_file):
     reg = PhotonRegistry()
     reg.add_module(os.path.join(current_path, "photonai_graph.json"))
     with open(os.path.join(registered_file), "w") as f:
-        f.write(str(datetime.now()))
+        f.write(str(__version__))
+
+
+def register():
+    current_path = os.path.dirname(os.path.abspath(__file__))
+    registered_file = os.path.join(current_path, "registered")
+    logger.info("Checking PHOTONAI Graph Module Registration")
+    if not os.path.isfile(registered_file):  # pragma: no cover
+        logger.info("Registering Graph Module")
+        do_register(current_path=current_path, registered_file=registered_file)
+    else:
+        with open(os.path.join(registered_file), "r") as f:
+            if f.read() == __version__:
+                logger.info("Current version already registered")
+            else:
+                logger.info("Updating Graph Module")
+                do_register(current_path=current_path, registered_file=registered_file)
+
+
+register()
